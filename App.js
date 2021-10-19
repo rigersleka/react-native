7c7d1eb965375582511b3856d2a86c8c0d893375
Rigers Leka <rigers.leka@sisal.al>
2021-10-19 12:48:50 +0200

    configure appLoading feature

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+
+const fetchFonts = () => {
+  return Font.loadAsyn({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setFontLoaded(true);
+        }}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js</Text>
-      <StatusBar style="auto" />
+      <Text>Welcome to React-Native App</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
